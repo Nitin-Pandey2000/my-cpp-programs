7019fd0d51a8f61ba626f7c7589b23c8dab2563d
nitin <nitinpandey.oct@gmail.com>
2022-06-22 16:25:59 +0530

    modified changes

--- datatypes.cpp
+++ datatypes.cpp
@@ -1,9 +1,24 @@
 #include <iostream> 
-using namespace std;   
+using namespace std; 
+void func(){
+        cout<< "This is String came from a fuction, which was defined by you."<<endl;
+}
 int main(){
-   int age=21,new_age;
-   char name[]="Nitin Pandey";
+    // regarding the datatype like (int,char,float)
+   int age=21,new_age,arr[5]={12,1,2,4,11},i;
+   float avg;
+   char name[]="Nitin Pandey",str[20];
    cout << "Hello " << name<<"\nyour age is: "<< age<<"\nWhat's your new age: ";
    cin>>new_age;
-   cout<<"this is your entered new age: "<<new_age<<"\n";
+   avg=(float)(age+new_age)/2;
+   cout<<"this is your entered new age: "<<new_age<<"\n"<<"Your average age is: "<<avg<<"\n";
+   cout<<"Your array: {";
+   for(i=0;i<5;i++){
+       cout<<arr[i];
+       if(i!=4){
+        cout<<",";
+       }
+   }
+   cout<<"}"<<endl;
+   func();
 }
