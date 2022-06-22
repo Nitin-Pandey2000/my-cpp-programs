843fc054d6d9aa92fbf4bc0e4a2f1f9dfc0ea44a
nitin <nitinpandey.oct@gmail.com>
2022-06-22 18:08:00 +0530

    updated

--- datatypes.cpp
+++ datatypes.cpp
@@ -5,16 +5,10 @@ void func(){
 }
 int main(){
     // regarding the datatype like (int,char,float)
-<<<<<<< HEAD
    int age=21,new_age,arr[5]={12,1,2,4,11},i;  // It'll take integer type values.
    float avg;                                  // It'll take floting point values.
    char name[]="Nitin Pandey";                 // Char is a datatype which will take single character as value and can take whole string with the spaces if array is provided as variable to it.
    string str;                                 // string datatype will take the string as value. And it'll be terminated whenever space is given.
-=======
-   int age=21,new_age,arr[5]={12,1,2,4,11},i;
-   float avg;
-   char name[]="Nitin Pandey";
->>>>>>> 860acca1a48af7261388d7eb40564e8438f26837
    cout << "Hello " << name<<"\nyour age is: "<< age<<"\nWhat's your new age: ";
    cin>>new_age;
    avg=(float)(age+new_age)/2;
@@ -28,10 +22,6 @@ int main(){
    }
    cout<<"}"<<endl;
    func();
-<<<<<<< HEAD
    cin>>str;
    cout<<"the string is: "<<str<<endl;
 }
-=======
-}
->>>>>>> 860acca1a48af7261388d7eb40564e8438f26837
