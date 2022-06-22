ef996d1f8112da98f4570093e3579097f91058ab
nitin <nitinpandey.oct@gmail.com>
2022-06-22 12:33:42 +0530

    New Commit

---
+++ datatypes.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;   
+int main(){
+   int age=21,new_age;
+   char name[]="Nitin Pandey";
+   cout << "Hello " << name<<"\nyour age is: "<< age<<"\nWhat's your new age: ";
+   cin>>new_age;
+   cout<<"this is your entered new age: "<<new_age<<"\n";
+}
